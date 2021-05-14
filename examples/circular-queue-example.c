4f483925fd74396ddc7864861b1b648942b4032e
harsath <harsath@tuta.io>
2021-05-14 18:41:12 -0400

    examples: (#29) cap_circular_queue usage examples, bug fixes

---
+++ examples/circular-queue-example.c
@@ -0,0 +1,44 @@
+#include "../cap-containers/circular_queue.h"
+
+int main(int argc, const char *argv[]) {
+	// Please Take a look at 'cap-containers/dynamic_queue.h' source file for all interface
+
+	// Allocating a container of size 3
+	cap_circular_queue *cqueue = cap_circular_queue_init(3);
+	int one = 10;
+	int two = 20;
+	int three = 30;
+	cap_circular_queue_push(cqueue, &one);
+	cap_circular_queue_push(cqueue, &two);
+
+	if (cap_circular_queue_size(cqueue) == 2) {
+		printf("Currently the container stores 2 elements\n");
+	}
+
+	cap_circular_queue_push(cqueue, &three);
+
+	int *one_ptr = cap_circular_queue_pop(cqueue);
+	int *two_ptr = cap_circular_queue_pop(cqueue);
+	int *three_ptr = cap_circular_queue_pop(cqueue);
+
+	if (cap_circular_queue_size(cqueue) == 0) {
+		printf("Currently the container stores 0 element\n");
+	}
+
+	if (*one_ptr == one) { printf("1st element poped was one\n"); }
+
+	if (*two_ptr == two) { printf("2nd element if poped was two\n"); }
+
+	if (*three_ptr == three) { printf("Final element poped was three\n"); }
+
+	cap_circular_queue_free(cqueue);
+
+	/* Other supported APIs:
+	 *	-> size_t cap_circular_queue_capacity(cap_circular_queue*)
+	 *		Returns the capacity of the container.
+	 *	-> void cap_circular_queue_deep_free(cap_circular_queue*)
+	 *		Free the container and also the underlying elements.
+	 */
+
+	return 0;
+}
--- tests/test-circular-queue.c
+++ tests/test-circular-queue.c
@@ -26,5 +26,19 @@ void test_circular_queue(void) {
 		cap_circular_queue_push(cqueue, &five);
 		CAP_ASSERT_EQ(*(int *)cap_circular_queue_pop(cqueue), five,
 			      "CIRCULAR_QUEUE 1st pop after 5 insert");
+		CAP_ASSERT_EQ(cap_circular_queue_size(cqueue), 4,
+			      "CIRCULAR_QUEUE size after 1st pop");
+		CAP_ASSERT_EQ(*(int *)cap_circular_queue_pop(cqueue), one,
+			      "CIRCULAR_QUEUE 2nd pop after 5 insert");
+		CAP_ASSERT_EQ(cap_circular_queue_size(cqueue), 3,
+			      "CIRCULAR_QUEUE size after 2nd pop");
+		CAP_ASSERT_EQ(*(int *)cap_circular_queue_pop(cqueue), three,
+			      "CIRCULAR_QUEUE 3rd pop after 5 insert");
+		CAP_ASSERT_EQ(*(int *)cap_circular_queue_pop(cqueue), four,
+			      "CIRCULAR_QUEUE 4th pop after 5 insert");
+		CAP_ASSERT_EQ(cap_circular_queue_size(cqueue), 1,
+			      "CIRCULAR_QUEUE size after 4th pop");
+		CAP_ASSERT_EQ(*(int *)cap_circular_queue_pop(cqueue), five,
+			      "CIRCULAR_QUEUE 4th pop after 5 insert");
 	}
 }
