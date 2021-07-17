d0b123c22a88612b82b23316b558a0825b799e39
harsath <harsath@tuta.io>
2021-07-16 21:23:42 -0400

    cap-containers: added support for priority-queue(#30)
    Still there is a minor change that we need to make since we're leaking
    the memory for the cap_priority_queue container itself, although we're
    freeing up the memory for the _internal_buffer. We'd need to figureout
    where the issue (double free or memory corruption) is happening.

--- tests/test-main.c
+++ tests/test-main.c
@@ -1,5 +1,4 @@
 #include "internal/test-helper.h"
-#include <stdio.h>
 extern void test_dynamic_queue(void);
 extern void test_fixed_queue(void);
 extern void test_list(void);
@@ -10,6 +9,7 @@ extern void test_hash_table_separate_chain(void);
 extern void test_map(void);
 extern void test_circular_queue(void);
 extern void test_arena_allocator(void);
+extern void test_priority_queue(void);
 
 int main(int argc, const char *const argv[]) {
 	test_dynamic_queue();
@@ -22,6 +22,7 @@ int main(int argc, const char *const argv[]) {
 	test_map();
 	test_circular_queue();
 	test_arena_allocator();
+	test_priority_queue();
 
 	return 0;
 }
